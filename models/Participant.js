4d96f7ff69543dd456423b35a9013c795915fae1
Md Abu Hasib Mahmud <57803694+iftymahmud@users.noreply.github.com>
2024-10-20 16:24:27 -0400

    Answer Panel Fixes

--- models/Participant.js
+++ models/Participant.js
@@ -1,11 +1,22 @@
 // models/Participant.js
 const mongoose = require('mongoose');
 
+const VoteSchema = new mongoose.Schema({
+  questionNumber: { type: Number, required: true },  
+  selectedOption: { type: String, required: true }, 
+});
+
+const VoteSubmissionSchema = new mongoose.Schema({
+  voteSession: { type: mongoose.Schema.Types.ObjectId, ref: 'VoteSession', required: true }, 
+  votes: [VoteSchema], 
+});
+
 const ParticipantSchema = new mongoose.Schema({
   name: { type: String, required: true },
   emoji: { type: String, required: true },
   code: { type: String, required: true },
-  dateCreated: { type: Date, default: Date.now }
+  dateCreated: { type: Date, default: Date.now },
+  submissions: [VoteSubmissionSchema]  
 });
 
 module.exports = mongoose.model('Participant', ParticipantSchema);
--- routes/voteParticipation.js
+++ routes/voteParticipation.js
@@ -10,12 +10,20 @@ router.get('/takevote/:voteSessionCode', async (req, res) => {
     try {
       const code = req.params.voteSessionCode;
       const voteSession = await VoteSession.findOne({ code: code });
+
+      let submitFlag = 0; 
+      const participant = await Participant.findById(req.session.userId);
+      let lastQuestionNumber = participant.submissions?.at(-1)?.votes?.at(-1)?.questionNumber;
+    
+      if(lastQuestionNumber >= voteSession.voteFlag+1){
+        submitFlag = 1;
+      }
       
       if (voteSession.voteFlag >= voteSession.questions.length ||
         voteSession.voteFlag == -1) {
         res.redirect(`/takevote/end/${voteSession.code}`);
       } else {
-        res.render('answerPanel', {voteSession});
+        res.render('answerPanel', {voteSession, submitFlag});
       }
     } catch (error) {
       console.error(error);
@@ -28,26 +36,43 @@ router.get('/takevote/:voteSessionCode', async (req, res) => {
 router.post('/takevote/:voteSessionCode', async (req, res) => {
     try {
       const code = req.params.voteSessionCode;
+      const selectedOption = req.body.selectedOption;
+
+
       const voteSession = await VoteSession.findOne({ code: code });
-      
-      if (voteSession.voteFlag >= voteSession.questions.length ||
-        voteSession.voteFlag == -1) {
-        res.redirect(`/takevote/end/${voteSession.code}`);
-      } else {
-        res.render('answerPanel', {voteSession});
+      const participant = await Participant.findById(req.session.userId);
+      const currentQuestion = voteSession.questions[voteSession.voteFlag];
+
+      if (!participant.submissions) {
+        participant.submissions = [];
       }
+  
+      let submission = participant.submissions.find(sub => sub.voteSession.equals(voteSession._id));
+  
+      if (!submission) {
+        submission = {
+          voteSession: voteSession._id,
+          votes: []
+        };
+        participant.submissions.push(submission);
+        await participant.save();
+        submission = participant.submissions.find(sub => sub.voteSession.equals(voteSession._id));
+      }
+  
+      const newVote = {
+        questionNumber: currentQuestion.questionNumber,
+        selectedOption
+      };
+  
+      submission.votes.push(newVote); 
+      await participant.save();
+      
+    res.redirect(`/takevote/${voteSession.code}`);
     } catch (error) {
       console.error(error);
       res.render('home', { error: 'An error occurred. Please try again.' });
     }
 });
-
-
-
-
-
-
-
 
 // GET Answer END Page
 router.get('/takevote/end/:voteSessionCode', async (req, res) => {
